68da7212a62f2929ca5413ed50538d82bbf8c04b
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2023-11-02 14:49:51 +0700

    configuation and build skeleton of project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout";
+import { SignInForm, SignUpForm } from "./_auth/form";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+import "./globals.css";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
