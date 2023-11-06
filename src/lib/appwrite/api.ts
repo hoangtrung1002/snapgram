ace8d1d810d55a3027fcb8c277a0ecf8a0122a43
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2023-11-06 23:12:26 +0700

    fetures and UI of SignUpForm

---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,82 @@
+import { INewUser } from "@/types";
+import { ID, Query } from "appwrite";
+import { account, appwriteConfig, avatars, database } from "./config";
+
+export const createUserAccount = async (user: INewUser) => {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(newAccount.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      imageUrl: avatarUrl,
+      username: user.username,
+    });
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export const saveUserToDB = async (user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) => {
+  try {
+    const newUser = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signInAccount = async (user: {
+  email: string;
+  password: string;
+}) => {
+  try {
+    const session = await account.createEmailSession(user.email, user.password);
+    return session;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get();
+    if (!currentAccount) throw Error;
+
+    const currentUser = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+  }
+};
